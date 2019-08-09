d89345f496b60e6a7afd9243d47a0e5fd687cbca
Adith A Danthi <adith.danthi@gmail.com>
2019-08-09 19:07:24 +0530

    Login UI created

--- app/src/main/java/com/example/vvce_staff/MainActivity.java
+++ app/src/main/java/com/example/vvce_staff/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.vvce_staff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText emailEt,passEt;
+    RadioGroup usersRg;
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEt = findViewById(R.id.email_et);
+        passEt =  findViewById(R.id.password_et);
+        usersRg =  findViewById(R.id.user_rg);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailEt.setVisibility(View.VISIBLE);
+                passEt.setVisibility(View.VISIBLE);
+                usersRg.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
+
+
 }
