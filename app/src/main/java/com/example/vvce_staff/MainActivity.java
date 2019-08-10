4ea66cfab387be17fa259a1ebcf3f8bac82772ce
Adith A Danthi <adith.danthi@gmail.com>
2019-08-10 13:21:32 +0530

    Added UI for registration

--- app/src/main/java/com/example/vvce_staff/MainActivity.java
+++ app/src/main/java/com/example/vvce_staff/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.vvce_staff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailEt,passEt;
     RadioGroup usersRg;
-    Button loginBtn;
+    Button loginBtn,registerBtn,signInBtn;
+    private String emailId,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
         emailEt = findViewById(R.id.email_et);
         passEt =  findViewById(R.id.password_et);
+
         usersRg =  findViewById(R.id.user_rg);
+
         loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.register_btn);
+        signInBtn = findViewById(R.id.sign_in_btn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +34,30 @@ public class MainActivity extends AppCompatActivity {
                 emailEt.setVisibility(View.VISIBLE);
                 passEt.setVisibility(View.VISIBLE);
                 usersRg.setVisibility(View.VISIBLE);
+                signInBtn.setVisibility(View.VISIBLE);
+
+                registerBtn.setVisibility(View.GONE);
+                loginBtn.setVisibility(View.GONE);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    emailId = emailEt.getText().toString();
+                    password = passEt.getText().toString();
+                } catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
---
+++ app/src/main/java/com/example/vvce_staff/RegistrationActivity.java
@@ -0,0 +1,83 @@
+package com.example.vvce_staff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class RegistrationActivity extends AppCompatActivity {
+    EditText fullNameEt,emailEt,passwordEt,confirmPasswordEt;
+    EditText facultyIdEt, facultyDesignationEt, usnEt;
+    RadioButton studentRb,facultyRb;
+    Button registerBtn;
+    private String fullName,email,usn,facultyId,facultyDesignation;
+    private String password,confirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        fullNameEt = findViewById(R.id.full_name_et);
+        emailEt = findViewById(R.id.email_et);
+        passwordEt = findViewById(R.id.password_et);
+        confirmPasswordEt = findViewById(R.id.confirm_password_et);
+
+        facultyIdEt = findViewById(R.id.faculty_id_et);
+        facultyDesignationEt = findViewById(R.id.designation_et);
+
+        usnEt = findViewById(R.id.usn_et);
+
+        studentRb = findViewById(R.id.student_rb);
+        facultyRb = findViewById(R.id.faculty_rb);
+
+        registerBtn = findViewById(R.id.register_btn);
+
+        facultyRb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                facultyDesignationEt.setVisibility(View.VISIBLE);
+                facultyIdEt.setVisibility(View.VISIBLE);
+
+                usnEt.setVisibility(View.GONE);
+            }
+        });
+
+        studentRb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usnEt.setVisibility(View.VISIBLE);
+
+                facultyDesignationEt.setVisibility(View.GONE);
+                facultyIdEt.setVisibility(View.GONE);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    fullName = fullNameEt.getText().toString();
+                    email = emailEt.getText().toString();
+
+                    facultyId = facultyIdEt.getText().toString();
+                    facultyDesignation = facultyDesignationEt.getText().toString();
+                    usn = usnEt.getText().toString();
+
+                    password = passwordEt.getText().toString();
+                    confirmPassword = confirmPasswordEt.getText().toString();
+
+                } catch (Exception e){
+                    Toast.makeText(RegistrationActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                
+                if(!password.equals(confirmPassword)){
+                    Toast.makeText(RegistrationActivity.this, "Passwords don't match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
