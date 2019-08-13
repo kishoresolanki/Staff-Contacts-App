85200cc0b036c0a294295da82002e322c8c2c3a4
kishoresolanki <kishoreseervi643@gmail.com>
2019-08-13 18:52:39 +0530

    toolbar registrationActivity

--- app/src/main/java/com/example/vvce_staff/RegistrationActivity.java
+++ app/src/main/java/com/example/vvce_staff/RegistrationActivity.java
@@ -203,6 +203,7 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         if(item.getItemId()==android.R.id.home){
